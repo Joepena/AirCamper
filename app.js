e8e103d1bad9efb766d156ed622e934cb7bfb5ae
josephpendev@gmail.com <josephpendev@gmail.com>
2016-10-31 22:42:55 -0400

    mongoose added along with basic data persistence

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 var express = require("express"),
     app     = express(),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-  {name: "Salmon Creek", image: "https://farm6.staticflickr.com/5136/5391759757_dd33e4ecc8.jpg"},
-  {name: "Granite Hill", image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg"},
-  {name: "Mountain Goat's Rest", image: "https://farm4.staticflickr.com/3069/2942421645_38b206298a.jpg"}
-];
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground",campgroundSchema);
 
 app.get("/", function(req,res){
   res.render("landing");
 });
 
 app.get("/campgrounds",function(req,res) {
-
-  res.render("campgrounds",{campgrounds: campgrounds});
+  Campground.find({},function(err,allcampgrounds){
+        if (err) {
+            console.log(err);
+        }
+        else {
+          res.render("campgrounds", {campgrounds:allcampgrounds})
+        }
+  });
 });
+
 
 app.get("/campgrounds/new",function(req,res){
   res.render('new.ejs');
@@ -28,8 +40,15 @@ app.post("/campgrounds",function(req,res){
   var name = req.body.name;
   var image = req.body.image;
   var newcampground = {name: name, image: image}
-  campgrounds.push(newcampground);
-  res.redirect("/campgrounds");
+  //Create a new campground and save to Db
+  Campground.create(newcampground, function(err,newlycreated){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.redirect("/campgrounds");
+    }
+  });
 
 });
 
