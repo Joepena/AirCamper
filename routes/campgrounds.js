8904942eae986311bd6ff6c09878075edfb00c8d
josephpendev@gmail.com <josephpendev@gmail.com>
2016-11-16 16:32:12 -0500

    Authorization on campground update and deletion

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -20,7 +20,7 @@ router.post("/",isLoggedIn,function(req,res){
   var image = req.body.image;
   var desc = req.body.description;
   var author = {
-      id: req.user_id,
+      id: req.user._id,
       username: req.user.username
   };
   var newcampground = {name: name, image: image, description: desc, author: author}
@@ -55,16 +55,10 @@ router.get("/:id",function(req,res){
 });
 
 //EDIT Campground Route
-router.get("/:id/edit",function(req,res){
-  Campground.findById(req.params.id,function(err, foundCampground){
-    if (err) {
-      res.redirect("/campgrounds");
-    }
-    else{
-      res.render("campgrounds/edit",{campground:foundCampground});
-    }
-  });
-
+router.get("/:id/edit",checkCampgroundOwnership, function(req,res){
+    Campground.findById(req.params.id,function(err, foundCampground){
+          res.render("campgrounds/edit",{campground:foundCampground});    
+    });      
 });
 
 //UPDATE Campground Route
@@ -79,7 +73,7 @@ router.put("/:id/edit",function(req,res){
   });
 });
 //DESTROY Route
-router.delete("/:id",function(req,res){
+router.delete("/:id",checkCampgroundOwnership, function(req,res){
   Campground.findByIdAndRemove(req.params.id,function(err){
     if (err) {
       res.redirect("/campgrounds");
@@ -97,4 +91,22 @@ function isLoggedIn(req,res,next){
   res.redirect("/login");
 }
 
+function checkCampgroundOwnership(req,res,next){
+  if(req.isAuthenticated()){
+      Campground.findById(req.params.id,function(err, foundCampground){
+          if (err) {
+              res.redirect("back");
+          }
+          else{
+            if(foundCampground.author.id.equals(req.user._id)){
+               next();
+            }  else {
+                res.redirect("back");
+            } 
+          }
+      });
+  }else{
+    res.redirect("back");
+  }    
+}
 module.exports = router;
