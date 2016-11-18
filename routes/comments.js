f31f0e133aa5a6f6ec38386a6ac97c72562ba418
josephpendev@gmail.com <josephpendev@gmail.com>
2016-11-17 22:12:44 -0500

    added update route to edit comment

--- routes/comments.js
+++ routes/comments.js
@@ -41,8 +41,27 @@ router.post("/",isLoggedIn,function(req,res){
   });
 });
 
+// Comments Edit route
 router.get("/:comment_id/edit",function(req,res){
-  res.send("EDIT ROUTE");
+  Comment.findById(req.params.comment_id,function(err, foundComment){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {campground_id : req.params.id, comment : foundComment});
+    }
+  });
+
+});
+
+//Comments Update Route 
+router.put("/:comment_id",function(req,res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+    if(err){
+      res.redirect("back");
+    }else{
+      res.redirect("/campgrounds/"+req.params.id);
+    }
+  });
 });
 
 //Middleware
