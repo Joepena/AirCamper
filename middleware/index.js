2990727abf7559e31e372f7cd25752037f460d65
josephpendev@gmail.com <josephpendev@gmail.com>
2016-11-23 14:44:36 -0500

    added error flash messaging to all CRUD errors

--- middleware/index.js
+++ middleware/index.js
@@ -5,12 +5,14 @@ middlewareObj.checkCampgroundOwnership = function (req,res,next){
   if(req.isAuthenticated()){
       Campground.findById(req.params.id,function(err, foundCampground){
           if (err) {
+              req.flash("error","Campground not found");
               res.redirect("back");
           }
           else{
             if(foundCampground.author.id.equals(req.user._id)){
                next();
             }  else {
+                req.flash("error","You don't have permission to do that.");
                 res.redirect("back");
             } 
           }
@@ -30,6 +32,7 @@ middlewareObj.checkCommentOwnership = function (req,res,next){
             if(foundComment.author.id.equals(req.user._id)){
                next();
             }  else {
+                req.flash("error","You need to be logged in to do that");
                 res.redirect("back");
             } 
           }
@@ -43,7 +46,7 @@ middlewareObj.isLoggedIn = function (req,res,next){
   if (req.isAuthenticated()) {
     return next();
   }
-  req.flash("error","Please Login First!");
+  req.flash("error","You need to be logged in to do that.");
   res.redirect("/login");
 }
 
